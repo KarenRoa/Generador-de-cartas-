3aa0ca62b561e4508a3679bc555ac01338db0beb
KarenRoa <roaaguayokaren@gmail.com>
2021-10-26 12:32:41 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function myfunction() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palo = Math.floor(Math.random() * pintas.length);
+  let numerito = Math.floor(Math.random() * numeros.length);
+
+  let aleatorias = pintas[palo] + " " + numeros[numerito];
+
+  console.log(aleatorias);
+  document.getElementsByTagName("body").innerHTML = aleatorias;
 };
+var boton = document.getElementById("myBtn");
